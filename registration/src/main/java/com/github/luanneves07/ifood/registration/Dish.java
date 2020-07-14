df41354c1f33211a96bb6dc0e7ce531283a27824
luanneves07 <luannevessilva@gmail.com>
2020-07-14 20:30:46 -0300

    QUARKUS-IFOOD - Creates restaurants CRUD

---
+++ registration/src/main/java/com/github/luanneves07/ifood/registration/Dish.java
@@ -0,0 +1,36 @@
+package com.github.luanneves07.ifood.registration;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "restaurant")
+public class Dish extends PanacheEntityBase {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+
+	public String name;
+
+	public BigDecimal price;
+
+	public String description;
+
+	@ManyToOne
+	public Restaurant restaurant;
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + "<" + id + ">";
+	}
+
+}
---
+++ registration/src/main/java/com/github/luanneves07/ifood/registration/RestaurantResource.java
@@ -0,0 +1,65 @@
+package com.github.luanneves07.ifood.registration;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/restaurants")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestaurantResource {
+
+	@GET
+	public List<Restaurant> index() {
+		return Restaurant.listAll();
+	}
+
+	@POST
+	@Transactional
+	public Response create(Restaurant dto) {
+		dto.persist();
+		return Response.status(Status.CREATED).build();
+	}
+
+	@PUT
+	@Path("{id}")
+	@Transactional
+	public Response update(@PathParam("id") Long id, Restaurant dto) {
+		Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(id);
+		if (restaurantOp.isEmpty()) {
+			return Response.status(Status.NOT_FOUND).build();
+		} else {
+			Restaurant restaurant = restaurantOp.get();
+			restaurant.name = dto.name;
+			restaurant.owner = dto.owner;
+			restaurant.persist();
+			return Response.status(Status.OK).build();
+		}
+	}
+
+	@DELETE
+	@Path("{id}")
+	@Transactional
+	public Response delete(@PathParam("id") Long id) {
+		Optional<Restaurant> restaurantOp = Restaurant.findByIdOptional(id);
+		if (restaurantOp.isEmpty()) {
+			return Response.status(Status.NOT_FOUND).build();
+		} else {
+			restaurantOp.get().delete();
+			return Response.status(Status.OK).build();
+		}
+	}
+}
